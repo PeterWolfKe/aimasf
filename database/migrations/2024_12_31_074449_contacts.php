8a7d4e155659856d9ce75ddd4dea40bf6bb97f3f
Julecko <julecko@dilino.sk>
2024-12-31 08:53:43 +0100

    Started working on database

---
+++ database/migrations/2024_12_31_074449_contacts.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('apartment_suite')->nullable();
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('phone')->nullable();
+            $table->string('delivery_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
