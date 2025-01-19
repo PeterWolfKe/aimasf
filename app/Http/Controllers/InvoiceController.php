96f128018d2aba7cb63286f7287f5d7d5c1afd2e
Julecko <julkoklein@gmail.com>
2025-01-19 04:10:44 +0100

    Started working on invoices

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\Product;
-use Barryvdh\DomPDF\Facade\Pdf as PDF;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use ConsoleTVs\Invoices\Classes\Invoice;
 
 class InvoiceController extends Controller
 {
@@ -15,54 +15,45 @@ class InvoiceController extends Controller
 
         $productDetails = json_decode($order->products, true);
 
-        $productsWithDetails = array_map(function ($productDetail) {
+        $invoice = Invoice::make()
+            ->number($order->unique_order_id)
+            ->due_date(Carbon::parse($order->created_at)->addDays(30))
+            ->customer([
+                'name'      => 'Èrik Campobadal Forés',
+                'id'        => '12345678A',
+                'phone'     => '+34 123 456 789',
+                'location'  => 'C / Unknown Street 1st',
+                'zip'       => '08241',
+                'city'      => 'Manresa',
+                'country'   => 'Spain',
+            ])
+            ->notes('Thank you for your purchase!');
+
+        foreach ($productDetails as $productDetail) {
             $product = Product::find($productDetail['id']);
 
             if ($product) {
                 $productImages = json_decode($product->product_images, true);
                 $image = $productImages[0] ?? null;
 
-                return [
-                    'id' => $product->id,
-                    'name' => $product->name,
-                    'description' => $product->description,
-                    'price' => $product->price,
-                    'size' => $productDetail['size'] ?? 'N/A',
-                    'quantity' => $productDetail['quantity'],
-                    'image' => $image
-                ];
+                $invoice->addItem(
+                    $product->name,
+                    $product->price,
+                    $productDetail['quantity'],
+                    $productDetail['quantity'] * $product->price,
+                );
             }
-
-            return null;
-        }, $productDetails);
-
-        $totalAmount = 0;
-        foreach ($productsWithDetails as $product) {
-            $totalAmount += $product['quantity'] * $product['price'];
         }
-
-        $pdf = PDF::loadView('invoice', [
-            'company_name' => 'Aimasf',
-            'company_address' => 'Poštová 9',
-            'company_city_postcode' => 'Košice, 042 42',
-            'company_phone' => '+421 919 195 062',
-            'company_email' => 'aima@aimasf.sk',
-            'invoice_number' => $order->unique_order_id,
-            'invoice_date' => $order->created_at->format('d-m-Y'),
-            'customer_name' => $order->first_name . ' ' . $order->last_name,
-            'customer_email' => $order->email,
-            'customer_address' => $order->address . ($order->apartment_suite ? ', ' . $order->apartment_suite : ''),
-            'customer_postal_code' => $order->postal_code,
-            'customer_city' => $order->city,
-            'customer_phone' => $order->phone,
-            'delivery_method' => $order->delivery_method,
-            'products' => $productsWithDetails,
-            'discount_code' => $order->discount_code,
-            'total_amount' => $totalAmount
+        $invoice->business([
+            'name' => 'Aimasf',
+            'address' => 'Poštová 9',
+            'city' => 'Košice',
+            'postal_code' => '042 42',
+            'phone' => '+421 919 195 062',
+            'email' => 'aima@aimasf.sk',
+            'website' => 'https://www.aimasf.sk',
         ]);
 
-        $pdf->setOptions(['isHtml5ParserEnabled' => true, 'isPhpEnabled' => true]);
-
-        return $pdf->download('invoice.pdf');
+        return $invoice->download('invoice_' . $order->unique_order_id . '.pdf');
     }
 }
